b8a48fc3fca3670fe4eddf44df83bf46e7f65e64
yjm <727787745@qq.com>
2017-08-11 23:33:08 +0800

    添加 *更新对象信息 *删除对象信息

--- src/RetrofitApi.java
+++ src/RetrofitApi.java
@@ -5,9 +5,11 @@ import retrofit2.http.*;
 
 /**
  * Created by jm on 2017/8/10.
- *
+ * retrofit接口
+ * Bmob全案例
  */
 public interface RetrofitApi {
+
     //https://api.bmob.cn/1/classes/Project
     @Headers({"x-bmob-application-id:fc845d2c89dcdab10aad9b17d4805f79",
             "x-bmob-rest-api-key:cc261ab628d365f1788c9fbb5a0be2e1",
@@ -22,4 +24,18 @@ public interface RetrofitApi {
             "content-type:application/json"})
     @POST("/1/classes/Project/")
     Call<ResponseBody> post(@Body RequestBody requestBody);
+
+    //https://api.bmob.cn/1/classes/TableName/objectId
+    @Headers({"x-bmob-application-id:fc845d2c89dcdab10aad9b17d4805f79",
+            "x-bmob-rest-api-key:cc261ab628d365f1788c9fbb5a0be2e1",
+            "content-type:application/json"})
+    @PUT("/1/classes/Project/{id}")
+    Call<ResponseBody> put(@Path("id") String id,@Body RequestBody requestBody);
+
+    //https://api.bmob.cn/1/classes/TableName/objectId
+    @Headers({"x-bmob-application-id:fc845d2c89dcdab10aad9b17d4805f79",
+            "x-bmob-rest-api-key:cc261ab628d365f1788c9fbb5a0be2e1",
+            "content-type:application/json"})
+    @DELETE("/1/classes/Project/{id}")
+    Call<ResponseBody> delete(@Path("id") String id);
 }
--- src/putInfoToClass3.java
+++ src/putInfoToClass3.java
@@ -3,23 +3,25 @@ import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+
 import java.io.IOException;
 
 import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 /**
- * Created by jm on 2017/8/10.
- * 添加对象信息
+ * Created by jm on 2017/8/11.
+ * 更新对象中的json格式的参数
  */
-public class postInfoToClass {
+public class putInfoToClass3 {
     static public void main(String[]args) throws IOException {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.bmob.cn/")
                 .build();
         RetrofitApi retrofitApi = retrofit.create(RetrofitApi.class);
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\n\t\"url\":\"http://ww4.sinaimg.cn/large/7a8aed7bgw1esfbgw6vc3j20gy0pedic.jpg\",\n\t\"subtitle\":\"不是美女4\"\n}");
-        Response<ResponseBody> response = retrofitApi.post(body).execute();
+        RequestBody body = RequestBody.create(mediaType, "{\"http.gender\":\"女\"}");
+        Response<ResponseBody> response = retrofitApi.put("4dc6ea83c1",body).execute();
         LOGGER.info(response.body().string());
+        //{"updatedAt":"2017-08-11 22:35:11"}
     }
 }
